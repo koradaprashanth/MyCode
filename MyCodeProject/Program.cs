8282b4198ac212256c94bfef15404a8ac4457034
PrashanthKorada <korada.prashanth@gmail.com>
2019-09-09 13:12:21 -0500

    init

---
+++ MyCodeProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyCodeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
